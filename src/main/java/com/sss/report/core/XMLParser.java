9441526c13697b0e76affd0be922d371507fc0e6
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-07-19 00:59:30 +0530

    Parsing each file in a dedicated thread

--- src/main/java/com/sss/report/core/XMLParser.java
+++ src/main/java/com/sss/report/core/XMLParser.java
@@ -1,24 +1,32 @@
 package com.sss.report.core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import com.sss.report.dao.FieldPermissionsDAO;
+import com.sss.report.dao.ProfileDAO;
 import com.sss.report.entity.FieldPermissionsEntity;
 import com.sss.report.entity.ProfileEntity;
 
-public class XMLParser implements Callable<Set<FieldPermissionsEntity>> {
+public class XMLParser implements Callable<ProfileEntity> {
+	
+	private String xmlFilePath;
 
 	private XMLStreamReader xmlStreamReader;
-
-	private  ProfileEntity profile;
-
-	public XMLParser(XMLStreamReader xmlStreamReader, ProfileEntity profile) {
-		this.xmlStreamReader = xmlStreamReader;
-		this.profile = profile;
+	
+	public XMLParser(String xmlFilePath) {
+		this.xmlFilePath = xmlFilePath;
 	}
 
 	private Boolean isFieldPermissions(String name) {
@@ -26,15 +34,36 @@ public class XMLParser implements Callable<Set<FieldPermissionsEntity>> {
 		return name.equalsIgnoreCase(target);
 	}
 
+	
+	private void configureXMLParser() throws IllegalArgumentException, FileNotFoundException, FactoryConfigurationError, XMLStreamException{
+		XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+		xmlInputFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, false);
+		InputStream xmlInputStream = new FileInputStream(xmlFilePath);
+		xmlStreamReader = xmlInputFactory.createXMLStreamReader(xmlInputStream) ;
+	}
+	
+	private String getXMLFileName() {
+		String xmlFileName = xmlFilePath.substring(xmlFilePath.lastIndexOf(File.separatorChar) + 1);
+		return xmlFileName;
+	}
+	
 	@Override
-	public Set<FieldPermissionsEntity> call() throws Exception {
+	public ProfileEntity call() throws Exception {
 		// TODO Auto-generated method stub
+		configureXMLParser();
 		Set<FieldPermissionsEntity> fieldPermissionsSet = new LinkedHashSet<>();
 		Boolean relatedToFieldPermissions = Boolean.FALSE;
+		ProfileDAO profileDAO = new ProfileDAO();
+		FieldPermissionsDAO fieldPermissionsDAO = new FieldPermissionsDAO();
 		FieldPermissionsEntity fieldPermissions = new FieldPermissionsEntity();
+		ProfileEntity profile = new ProfileEntity();
 		String name = "";
 		while (xmlStreamReader.hasNext()) {
-			switch (xmlStreamReader.next()) {
+			switch (xmlStreamReader.getEventType()) {
+			case XMLStreamReader.START_DOCUMENT:
+				profile.setName(getXMLFileName());
+				System.out.println(profile.getName() + " " + profileDAO.create(profile));
+				break;
 			case XMLStreamReader.START_ELEMENT:
 				name = xmlStreamReader.getLocalName();
 				if(isFieldPermissions(name)) {
@@ -53,6 +82,8 @@ public class XMLParser implements Callable<Set<FieldPermissionsEntity>> {
 				name = xmlStreamReader.getLocalName();
 				if(isFieldPermissions(name)) {
 					fieldPermissions.setProfile(profile);
+					Integer id = fieldPermissionsDAO.create(fieldPermissions);
+					System.out.println(id + " " + fieldPermissions.getField());
 					fieldPermissionsSet.add(fieldPermissions);
 					relatedToFieldPermissions = Boolean.FALSE;
 				}
@@ -60,7 +91,6 @@ public class XMLParser implements Callable<Set<FieldPermissionsEntity>> {
 			case XMLStreamReader.CHARACTERS:
 				String value = xmlStreamReader.getText();
 				if (relatedToFieldPermissions && value.trim().length() != 0) {
-					//System.out.println(name + " = " + value);
 					Field field = FieldPermissionsEntity.class.getDeclaredField(name);
 					field.setAccessible(true);
 					try {
@@ -71,10 +101,14 @@ public class XMLParser implements Callable<Set<FieldPermissionsEntity>> {
 					field.setAccessible(false);
 				}
 				break;
+			case XMLStreamReader.END_DOCUMENT :
+				profile.setFieldPermissions(fieldPermissionsSet);
+				break;
 			}
+			xmlStreamReader.next();
 		}
 		xmlStreamReader.close();
-		return fieldPermissionsSet;
+		return profile;
 	}
 
 }
