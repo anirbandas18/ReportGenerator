db92f49dbf2c1d03ca010ebeecbddbfb98734436
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-07-19 16:03:55 +0530

    Specified changed main class name in pom.xml for packaging into jar

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 set /p mode= Provide run mode 'profile' or 'property' : 
 set /p input= Input directory path : 
 set /p output= Output directory path : 
-java -Xms1024m -Xns512m -Xmx1024m -jar target/ReportGenerator-0.0.1-SNAPSHOT.jar %mode% %input% %output%
+java -Xms1024m -Xmx1024m -jar target/ReportGenerator-0.1.1-SNAPSHOT.jar %mode% %input% %output%
 pause
